ef653a4ccd895a5ac5d8d12497b6e1fef750a797
lightbulbs <lightbulbs@662d3e3c-bab5-4bb8-927d-f9ea1456bd5c>
2014-04-08 05:49:34 +0000

    Bricksmith 3.0 tag

---
+++ tags/Bricksmith3.0/Bricksmith/Source/LDraw/Files/LDrawContainer.h
@@ -0,0 +1,54 @@
+//==============================================================================
+//
+// File:		LDrawContainer.m
+//
+// Purpose:		Abstract subclass for LDrawDirectives which represent a 
+//				collection of related directives.
+//
+//  Created by Allen Smith on 3/31/05.
+//  Copyright (c) 2005. All rights reserved.
+//==============================================================================
+#import <Foundation/Foundation.h>
+
+#import "LDrawDirective.h"
+#import "MatrixMath.h"
+
+@class PartReport;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Class:		LDrawContainer
+//
+////////////////////////////////////////////////////////////////////////////////
+@interface LDrawContainer : LDrawDirective <NSCoding, NSCopying, LDrawObserver>
+{
+	@protected
+	BOOL				postsNotifications;
+	
+	@private
+	NSMutableArray		*containedObjects;
+}
+
+//Accessors
+- (NSArray *) allEnclosedElements;
+- (Box3) projectedBoundingBoxWithModelView:(Matrix4)modelView
+								projection:(Matrix4)projection
+									  view:(Box2)viewport;
+- (NSInteger) indexOfDirective:(LDrawDirective *)directive;
+- (NSMutableArray *) subdirectives;
+
+- (void) setPostsNotifications:(BOOL)flag;
+- (void) setVertexesNeedRebuilding;
+- (void) setSubdirectiveSelected:(BOOL)flag;
+
+//Actions
+- (void) addDirective:(LDrawDirective *)directive;
+- (void) collectPartReport:(PartReport *)report;
+- (void) insertDirective:(LDrawDirective *)directive atIndex:(NSInteger)index;
+- (void) removeDirective:(LDrawDirective *)doomedDirective;
+- (void) removeDirectiveAtIndex:(NSInteger)index;
+
+- (BOOL) acceptsDroppedDirective:(LDrawDirective *)directive;
+- (void) optimizeVertexes;
+
+@end
